9590e1f180c8501b2d93669af109dd2cd680a1c2
ragnar <rahulgovind517@gmail.com>
2016-10-13 00:24:33 +0530

    More files removed

--- .emacs.d/elpa/linum-relative-0.4/linum-relative-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; linum-relative-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads "actual autoloads are elsewhere" "linum-relative"
-;;;;;;  "../../../../.emacs.d/elpa/linum-relative-0.4/linum-relative.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../.emacs.d/elpa/linum-relative-0.4/linum-relative.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "linum-relative" '("linum-relative")))
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/linum-relative-0.4/linum-relative-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/linum-relative-0.4/linum-relative.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; linum-relative-autoloads.el ends here
--- .emacs.d/elpa/linum-relative-0.4/linum-relative-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "linum-relative" "0.4" "display relative line number in emacs." 'nil :url "http://github.com/coldnew/linum-relative" :keywords '("converience"))
